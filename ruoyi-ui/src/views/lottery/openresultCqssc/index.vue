7466f8cfa078d6ad1e8379f31c11f96521fccafd
xiaoyu <xiaoyu@xiaoyudeiMac.local>
2024-04-15 19:00:22 +0700

    重庆时时彩

--- ruoyi-ui/src/views/lottery/openresultCqssc/index.vue
+++ ruoyi-ui/src/views/lottery/openresultCqssc/index.vue
@@ -73,14 +73,14 @@
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-            v-hasPermi="['lottery:openresultJsk3:edit']"
+            v-hasPermi="['lottery:openresultCqssc:edit']"
           >修改</el-button>
           <!-- <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['lottery:openresultJsk3:remove']"
+            v-hasPermi="['lottery:openresultCqssc:remove']"
           >删除</el-button> -->
         </template>
       </el-table-column>
