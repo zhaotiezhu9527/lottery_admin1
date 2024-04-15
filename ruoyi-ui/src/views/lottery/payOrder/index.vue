7cbcd4f3a85403160a1536a93a0f564a1c1f286b
xiaoyu <xiaoyu@xiaoyudeiMac.local>
2024-04-15 16:56:41 +0700

    支付订单

--- ruoyi-ui/src/views/lottery/payOrder/index.vue
+++ ruoyi-ui/src/views/lottery/payOrder/index.vue
@@ -17,7 +17,7 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="三方订单号" prop="payOrderNo">
+      <el-form-item label="三方订单" prop="payOrderNo">
         <el-input
           v-model="queryParams.payOrderNo"
           placeholder="请输入三方订单号"
@@ -25,53 +25,38 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="金额" prop="amount">
-        <el-input
-          v-model="queryParams.amount"
-          placeholder="请输入金额"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
+      <el-form-item label="商户名称">
+        <el-select v-model="queryParams.merchantCode" placeholder="请选择">
+          <el-option value="" label="所有商户"></el-option>
+          <el-option
+            v-for="item in payMerchantList"
+            :key="item.merchantCode"
+            :label="item.merchantName"
+            :value="item.merchantCode">
+          </el-option>
+        </el-select>
       </el-form-item>
-      <el-form-item label="商户代码" prop="merchantCode">
-        <el-input
-          v-model="queryParams.merchantCode"
-          placeholder="请输入商户代码"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
+      <el-form-item label="状态">
+        <el-select v-model="queryParams.status" placeholder="请选择">
+          <el-option
+            v-for="item in typeList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
       </el-form-item>
-      <el-form-item label="通道代码" prop="channelCode">
-        <el-input
-          v-model="queryParams.channelCode"
-          placeholder="请输入通道代码"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="商户名称" prop="merchantName">
-        <el-input
-          v-model="queryParams.merchantName"
-          placeholder="请输入商户名称"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="通道名称" prop="channelName">
-        <el-input
-          v-model="queryParams.channelName"
-          placeholder="请输入通道名称"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="签名" prop="sign">
-        <el-input
-          v-model="queryParams.sign"
-          placeholder="请输入签名"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
+      <el-form-item label="时间" prop="orderTime">
+        <el-date-picker
+          v-model="dateRange"
+          style="width: 340px"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          type="datetimerange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :picker-options="pickerOptions"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -82,38 +67,6 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['lottery:payOrder:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['lottery:payOrder:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['lottery:payOrder:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
           type="warning"
           plain
           icon="el-icon-download"
@@ -122,33 +75,30 @@
           v-hasPermi="['lottery:payOrder:export']"
         >导出</el-button>
       </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="payOrderList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="id" align="center" prop="id" />
       <el-table-column label="用户名" align="center" prop="userName" />
       <el-table-column label="订单号" align="center" prop="orderNo" />
-      <el-table-column label="三方订单号" align="center" prop="payOrderNo" />
-      <el-table-column label="金额" align="center" prop="amount" />
-      <el-table-column label="0:未支付 1:成功 2:失败" align="center" prop="status" />
-      <el-table-column label="备注" align="center" prop="remark" />
-      <el-table-column label="错误消息" align="center" prop="errorMsg" />
-      <el-table-column label="商户代码" align="center" prop="merchantCode" />
-      <el-table-column label="通道代码" align="center" prop="channelCode" />
       <el-table-column label="商户名称" align="center" prop="merchantName" />
       <el-table-column label="通道名称" align="center" prop="channelName" />
-      <el-table-column label="签名" align="center" prop="sign" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <el-table-column label="三方订单号" align="center" prop="payOrderNo" />
+      <el-table-column label="金额" align="center" prop="amount" >
         <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['lottery:payOrder:edit']"
-          >修改</el-button>
+          <span class="red-text">{{ scope.row.amount }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="支付状态" align="center" prop="status" >
+        <template slot-scope="scope">
+          <el-tag type="warning" v-if="scope.row.status === 0">未支付</el-tag>
+          <el-tag type="success" v-else-if="scope.row.status === 1">支付成功</el-tag>
+          <el-tag type="danger" v-else-if="scope.row.status === 2">支付失败</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" align="center" prop="createTime" />
+      <el-table-column label="备注" align="center" prop="remark" />
+      <!-- <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
           <el-button
             size="mini"
             type="text"
@@ -157,7 +107,7 @@
             v-hasPermi="['lottery:payOrder:remove']"
           >删除</el-button>
         </template>
-      </el-table-column>
+      </el-table-column> -->
     </el-table>
     
     <pagination
@@ -167,54 +117,13 @@
       :limit.sync="queryParams.pageSize"
       @pagination="getList"
     />
-
-    <!-- 添加或修改支付订单对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="用户名" prop="userName">
-          <el-input v-model="form.userName" placeholder="请输入用户名" />
-        </el-form-item>
-        <el-form-item label="订单号" prop="orderNo">
-          <el-input v-model="form.orderNo" placeholder="请输入订单号" />
-        </el-form-item>
-        <el-form-item label="三方订单号" prop="payOrderNo">
-          <el-input v-model="form.payOrderNo" placeholder="请输入三方订单号" />
-        </el-form-item>
-        <el-form-item label="金额" prop="amount">
-          <el-input v-model="form.amount" placeholder="请输入金额" />
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" placeholder="请输入备注" />
-        </el-form-item>
-        <el-form-item label="错误消息" prop="errorMsg">
-          <el-input v-model="form.errorMsg" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-        <el-form-item label="商户代码" prop="merchantCode">
-          <el-input v-model="form.merchantCode" placeholder="请输入商户代码" />
-        </el-form-item>
-        <el-form-item label="通道代码" prop="channelCode">
-          <el-input v-model="form.channelCode" placeholder="请输入通道代码" />
-        </el-form-item>
-        <el-form-item label="商户名称" prop="merchantName">
-          <el-input v-model="form.merchantName" placeholder="请输入商户名称" />
-        </el-form-item>
-        <el-form-item label="通道名称" prop="channelName">
-          <el-input v-model="form.channelName" placeholder="请输入通道名称" />
-        </el-form-item>
-        <el-form-item label="签名" prop="sign">
-          <el-input v-model="form.sign" placeholder="请输入签名" />
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import { listPayOrder, getPayOrder, delPayOrder, addPayOrder, updatePayOrder } from "@/api/lottery/payOrder";
+import { listPayOrder, delPayOrder } from "@/api/lottery/payOrder";
+import { listPayMerchant } from "@/api/lottery/payMerchant";
+import { pickerOptions } from '@/utils/auth'
 
 export default {
   name: "PayOrder",
@@ -222,22 +131,12 @@ export default {
     return {
       // 遮罩层
       loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
       // 显示搜索条件
       showSearch: true,
       // 总条数
       total: 0,
       // 支付订单表格数据
       payOrderList: [],
-      // 弹出层标题
-      title: "",
-      // 是否显示弹出层
-      open: false,
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -245,45 +144,33 @@ export default {
         userName: null,
         orderNo: null,
         payOrderNo: null,
-        amount: null,
-        status: null,
-        errorMsg: null,
-        merchantCode: null,
-        channelCode: null,
-        merchantName: null,
-        channelName: null,
-        sign: null
+        status: "",
+        merchantCode: "",
+        isAsc:'desc',
+        orderByColumn:'id'
       },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-        userName: [
-          { required: true, message: "用户名不能为空", trigger: "blur" }
-        ],
-        orderNo: [
-          { required: true, message: "订单号不能为空", trigger: "blur" }
-        ],
-      }
+      typeList: [
+        { label: '全部', value: ''},
+        { label: '启用', value: 0},
+        { label: '停用', value: 2},
+      ],//状态
+      pickerOptions: pickerOptions,
+      // 时间
+      dateRange:[],
     };
   },
   created() {
-    this.getList();
+    this.getMerchantList();
   },
   methods: {
     /** 查询支付订单列表 */
     getList() {
       this.loading = true;
-      listPayOrder(this.queryParams).then(response => {
+      listPayOrder(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
         this.payOrderList = response.rows;
         this.total = response.total;
         this.loading = false;
       });
-    },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
     },
     // 表单重置
     reset() {
@@ -316,48 +203,6 @@ export default {
       this.resetForm("queryForm");
       this.handleQuery();
     },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.id)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加支付订单";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const id = row.id || this.ids
-      getPayOrder(id).then(response => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改支付订单";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.id != null) {
-            updatePayOrder(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            addPayOrder(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
-          }
-        }
-      });
-    },
     /** 删除按钮操作 */
     handleDelete(row) {
       const ids = row.id || this.ids;
@@ -373,7 +218,18 @@ export default {
       this.download('lottery/payOrder/export', {
         ...this.queryParams
       }, `payOrder_${new Date().getTime()}.xlsx`)
-    }
+    },
+     /** 查询商家列表 */
+     getMerchantList() {
+      this.loading = true;
+      listPayMerchant({
+        pageNum: 1,
+        pageSize: 1000,
+      }).then(response => {
+        this.payMerchantList = response.rows;
+        this.getList();
+      });
+    },
   }
 };
 </script>
