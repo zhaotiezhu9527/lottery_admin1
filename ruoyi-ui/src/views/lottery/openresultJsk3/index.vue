84d7bc34b2a1d03161f880fbbdf8d59f6efb30c2
xiaoyu <xiaoyu@xiaoyudeiMac.local>
2024-04-15 18:40:01 +0700

    开奖结果，江苏快三，一分快三

--- ruoyi-ui/src/views/lottery/openresultJsk3/index.vue
+++ ruoyi-ui/src/views/lottery/openresultJsk3/index.vue
@@ -9,6 +9,18 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
+      <el-form-item label="时间" prop="orderTime">
+        <el-date-picker
+          v-model="dateRange"
+          style="width: 340px"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          type="datetimerange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :picker-options="pickerOptions"
+        ></el-date-picker>
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
@@ -16,38 +28,6 @@
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['lottery:openresultJsk3:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['lottery:openresultJsk3:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['lottery:openresultJsk3:remove']"
-        >删除</el-button>
-      </el-col>
       <el-col :span="1.5">
         <el-button
           type="warning"
@@ -63,23 +43,27 @@
 
     <el-table v-loading="loading" :data="openresultJsk3List">
       <el-table-column label="期号" align="center" prop="qs" />
-      <el-table-column label="三方期号" align="center" prop="platQs" />
       <el-table-column label="开奖号码" align="center" prop="openResult" />
-      <el-table-column label="0:已开奖 1:未开奖" align="center" prop="openStatus" />
+      <el-table-column label="开奖状态" align="center" prop="openStatus" >
+        <template slot-scope="scope">
+          <div v-if="scope.row.openStatus === 0">已开奖</div>
+          <div v-else-if="scope.row.openStatus === 1">未开奖</div>
+        </template>
+      </el-table-column>
       <el-table-column label="当前第N期" align="center" prop="currCount" />
       <el-table-column label="开盘时间" align="center" prop="openTime" width="180">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.openTime, '{y}-{m}-{d}') }}</span>
+          <span>{{ parseTime(scope.row.openTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="封盘时间" align="center" prop="closeTime" width="180">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.closeTime, '{y}-{m}-{d}') }}</span>
+          <span>{{ parseTime(scope.row.closeTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="开奖时间" align="center" prop="openResultTime" width="180">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.openResultTime, '{y}-{m}-{d}') }}</span>
+          <span>{{ parseTime(scope.row.openResultTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
@@ -91,13 +75,13 @@
             @click="handleUpdate(scope.row)"
             v-hasPermi="['lottery:openresultJsk3:edit']"
           >修改</el-button>
-          <el-button
+          <!-- <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['lottery:openresultJsk3:remove']"
-          >删除</el-button>
+          >删除</el-button> -->
         </template>
       </el-table-column>
     </el-table>
@@ -114,40 +98,10 @@
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="期号" prop="qs">
-          <el-input v-model="form.qs" placeholder="请输入期号" />
-        </el-form-item>
-        <el-form-item label="三方期号" prop="platQs">
-          <el-input v-model="form.platQs" placeholder="请输入三方期号" />
+          <el-input v-model="form.qs" :disabled="true" placeholder="请输入期号" />
         </el-form-item>
         <el-form-item label="开奖号码" prop="openResult">
           <el-input v-model="form.openResult" placeholder="请输入开奖号码" />
-        </el-form-item>
-        <el-form-item label="当前第N期" prop="currCount">
-          <el-input v-model="form.currCount" placeholder="请输入当前第N期" />
-        </el-form-item>
-        <el-form-item label="开盘时间" prop="openTime">
-          <el-date-picker clearable
-            v-model="form.openTime"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="请选择开盘时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="封盘时间" prop="closeTime">
-          <el-date-picker clearable
-            v-model="form.closeTime"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="请选择封盘时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="开奖时间" prop="openResultTime">
-          <el-date-picker clearable
-            v-model="form.openResultTime"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="请选择开奖时间">
-          </el-date-picker>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -159,7 +113,8 @@
 </template>
 
 <script>
-import { listOpenresultJsk3, getOpenresultJsk3, delOpenresultJsk3, addOpenresultJsk3, updateOpenresultJsk3 } from "@/api/lottery/openresultJsk3";
+import { listOpenresultJsk3, getOpenresultJsk3, delOpenresultJsk3, updateOpenresultJsk3 } from "@/api/lottery/openresultJsk3";
+import { dateFormat,pickerOptions } from '@/utils/auth'
 
 export default {
   name: "OpenresultJsk3",
@@ -167,12 +122,6 @@ export default {
     return {
       // 遮罩层
       loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
       // 显示搜索条件
       showSearch: true,
       // 总条数
@@ -188,13 +137,8 @@ export default {
         pageNum: 1,
         pageSize: 10,
         qs: null,
-        platQs: null,
-        openResult: null,
-        openStatus: null,
-        currCount: null,
-        openTime: null,
-        closeTime: null,
-        openResultTime: null,
+        isAsc:'asc',
+        orderByColumn:'open_result_time'
       },
       // 表单参数
       form: {},
@@ -203,17 +147,21 @@ export default {
         qs: [
           { required: true, message: "期号不能为空", trigger: "blur" }
         ],
-      }
+      },
+      pickerOptions: pickerOptions,
+      // 时间
+      dateRange:[],
     };
   },
   created() {
+    this.getDefaultTime();
     this.getList();
   },
   methods: {
     /** 查询开奖结果(江苏快3)列表 */
     getList() {
       this.loading = true;
-      listOpenresultJsk3(this.queryParams).then(response => {
+      listOpenresultJsk3(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
         this.openresultJsk3List = response.rows;
         this.total = response.total;
         this.loading = false;
@@ -229,15 +177,7 @@ export default {
       this.form = {
         id: null,
         qs: null,
-        platQs: null,
         openResult: null,
-        openStatus: null,
-        currCount: null,
-        openTime: null,
-        closeTime: null,
-        openResultTime: null,
-        createTime: null,
-        updateTime: null
       };
       this.resetForm("form");
     },
@@ -269,17 +209,15 @@ export default {
     },
     /** 提交按钮 */
     submitForm() {
+      let obj = {
+        id:this.form.id,
+        openResult:this.form.openResult,
+      }
       this.$refs["form"].validate(valid => {
         if (valid) {
           if (this.form.id != null) {
-            updateOpenresultJsk3(this.form).then(response => {
+            updateOpenresultJsk3(obj).then(response => {
               this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            addOpenresultJsk3(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
               this.open = false;
               this.getList();
             });
@@ -302,7 +240,14 @@ export default {
       this.download('lottery/openresultJsk3/export', {
         ...this.queryParams
       }, `openresultJsk3_${new Date().getTime()}.xlsx`)
-    }
+    },
+    getDefaultTime() {
+      let end = new Date();
+      let start = new Date();
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+      this.dateRange[0] = dateFormat("YYYY-mm-dd" , end) + ' 00:00:00'
+      this.dateRange[1] = dateFormat("YYYY-mm-dd" , end) + ' 23:59:59'
+    },
   }
 };
 </script>
