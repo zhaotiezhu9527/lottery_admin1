9bc9d3b76b965638b5edf4d4a0aac5c878c19645
lichade123321 <lichade123333@outlook.com>
2024-04-15 12:24:20 +0800

    实体类ID增加主键

--- ruoyi-lottery/src/main/java/com/ruoyi/lottery/domain/sbRecord.java
+++ ruoyi-lottery/src/main/java/com/ruoyi/lottery/domain/sbRecord.java
@@ -2,6 +2,9 @@ package com.ruoyi.lottery.domain;
 
 import java.math.BigDecimal;
 import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -23,6 +26,7 @@ public class sbRecord extends BaseEntity
     private static final long serialVersionUID = 1L;
 
     /** id */
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /** 用户名 */
