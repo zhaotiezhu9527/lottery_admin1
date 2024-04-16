48f13db0e052bf3aa9827461be355db78a8d351a
xiaoyu <xiaoyu@xiaoyudeiMac.local>
2024-04-16 12:53:54 +0700

    AG注单

--- ruoyi-ui/src/views/lottery/agRecord/index.vue
+++ ruoyi-ui/src/views/lottery/agRecord/index.vue
@@ -9,14 +9,6 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="平台用户名" prop="platUserName">
-        <el-input
-          v-model="queryParams.platUserName"
-          placeholder="请输入平台用户名"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
       <el-form-item label="注单号码" prop="orderNo">
         <el-input
           v-model="queryParams.orderNo"
@@ -25,10 +17,10 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="游戏ID" prop="gameId">
+      <el-form-item label="局号" prop="serialId">
         <el-input
-          v-model="queryParams.gameId"
-          placeholder="请输入游戏ID"
+          v-model="queryParams.serialId"
+          placeholder="请输入局号"
           clearable
           @keyup.enter.native="handleQuery"
         />
@@ -41,75 +33,27 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="有效投注" prop="effectiveBet">
-        <el-input
-          v-model="queryParams.effectiveBet"
-          placeholder="请输入有效投注"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="总投注额" prop="allBet">
-        <el-input
-          v-model="queryParams.allBet"
-          placeholder="请输入总投注额"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="派彩金额(不包含本金)" prop="profit">
-        <el-input
-          v-model="queryParams.profit"
-          placeholder="请输入派彩金额(不包含本金)"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="下注时间">
-        <el-date-picker
-          v-model="daterangeBetTime"
-          style="width: 240px"
-          value-format="yyyy-MM-dd"
-          type="daterange"
-          range-separator="-"
-          start-placeholder="开始日期"
-          end-placeholder="结束日期"
-        ></el-date-picker>
-      </el-form-item>
-      <el-form-item label="下注时间美东" prop="wagersDate">
-        <el-date-picker clearable
-          v-model="queryParams.wagersDate"
-          type="date"
-          value-format="yyyy-MM-dd"
-          placeholder="请选择下注时间美东">
-        </el-date-picker>
+      <el-form-item label="类型" prop="gameType">
+        <el-select v-model="queryParams.gameType" placeholder="请选择">
+          <el-option
+            v-for="item in gameList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="结算时间">
         <el-date-picker
-          v-model="daterangeSettleTime"
-          style="width: 240px"
-          value-format="yyyy-MM-dd"
-          type="daterange"
+          v-model="dateRange"
+          style="width: 340px"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          type="datetimerange"
           range-separator="-"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
+          :picker-options="pickerOptions"
         ></el-date-picker>
-      </el-form-item>
-      <el-form-item label="结算时间美东" prop="modifiedDate">
-        <el-date-picker clearable
-          v-model="queryParams.modifiedDate"
-          type="date"
-          value-format="yyyy-MM-dd"
-          placeholder="请选择结算时间美东">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="局号gameCode" prop="serialId">
-        <el-input
-          v-model="queryParams.serialId"
-          placeholder="请输入局号gameCode"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -120,38 +64,6 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['lottery:agRecord:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['lottery:agRecord:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['lottery:agRecord:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
           type="warning"
           plain
           icon="el-icon-download"
@@ -160,58 +72,44 @@
           v-hasPermi="['lottery:agRecord:export']"
         >导出</el-button>
       </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="agRecordList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
+    <el-table v-loading="loading" :data="agRecordList">
       <el-table-column label="用户名" align="center" prop="userName" />
-      <el-table-column label="平台用户名" align="center" prop="platUserName" />
-      <el-table-column label="注单号码" align="center" prop="orderNo" />
-      <el-table-column label="游戏ID" align="center" prop="gameId" />
+      <el-table-column label="注单号码" align="center" prop="orderNo" width="150"/>
+      <el-table-column label="局号" align="center" prop="serialId" width="150"/>
       <el-table-column label="游戏名称" align="center" prop="gameName" />
-      <el-table-column label="有效投注" align="center" prop="effectiveBet" />
-      <el-table-column label="总投注额" align="center" prop="allBet" />
-      <el-table-column label="派彩金额(不包含本金)" align="center" prop="profit" />
-      <el-table-column label="下注时间" align="center" prop="betTime" width="180">
+      <el-table-column label="类型" align="center" prop="gameType" >
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.betTime, '{y}-{m}-{d}') }}</span>
+          <span v-if="scope.row.gameType == 1">真人视讯</span>
+          <span v-else-if="scope.row.gameType == 2">电子游戏</span>
+          <span v-else-if="scope.row.gameType == 3">捕鱼</span>
         </template>
       </el-table-column>
-      <el-table-column label="下注时间美东" align="center" prop="wagersDate" width="180">
+      <el-table-column label="有效投注" align="center" prop="effectiveBet" />
+      <el-table-column label="总投注额" align="center" prop="allBet" />
+      <el-table-column label="输赢金额" align="center" prop="profit" >
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.wagersDate, '{y}-{m}-{d}') }}</span>
+          <span v-if="scope.row.profit > 0" class="red-text">{{ scope.row.profit }}</span>
+          <span v-else class="green-text">{{ scope.row.profit }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="下注时间" align="center" prop="betTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.betTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
         </template>
       </el-table-column>
       <el-table-column label="结算时间" align="center" prop="settleTime" width="180">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.settleTime, '{y}-{m}-{d}') }}</span>
+          <span>{{ parseTime(scope.row.settleTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="结算时间美东" align="center" prop="modifiedDate" width="180">
+      <el-table-column label="状态" align="center" prop="resultStatus" >
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.modifiedDate, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="0：异常(请联系客服) 1：已派彩 -8：取消指定局注单 -9：取消指定注单" align="center" prop="resultStatus" />
-      <el-table-column label="局号gameCode" align="center" prop="serialId" />
-      <el-table-column label="1:视讯 2:电子 3:捕鱼" align="center" prop="gameType" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['lottery:agRecord:edit']"
-          >修改</el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
-            v-hasPermi="['lottery:agRecord:remove']"
-          >删除</el-button>
+          <span v-if="scope.row.resultStatus == 0">异常(请联系客服)</span>
+          <span v-else-if="scope.row.resultStatus == 1">已派彩</span>
+          <span v-else-if="scope.row.resultStatus == -8">取消指定局注单</span>
+          <span v-else-if="scope.row.resultStatus == -9">取消指定注单</span>
         </template>
       </el-table-column>
     </el-table>
@@ -223,80 +121,12 @@
       :limit.sync="queryParams.pageSize"
       @pagination="getList"
     />
-
-    <!-- 添加或修改AG注单对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="用户名" prop="userName">
-          <el-input v-model="form.userName" placeholder="请输入用户名" />
-        </el-form-item>
-        <el-form-item label="平台用户名" prop="platUserName">
-          <el-input v-model="form.platUserName" placeholder="请输入平台用户名" />
-        </el-form-item>
-        <el-form-item label="注单号码" prop="orderNo">
-          <el-input v-model="form.orderNo" placeholder="请输入注单号码" />
-        </el-form-item>
-        <el-form-item label="游戏ID" prop="gameId">
-          <el-input v-model="form.gameId" placeholder="请输入游戏ID" />
-        </el-form-item>
-        <el-form-item label="游戏名称" prop="gameName">
-          <el-input v-model="form.gameName" placeholder="请输入游戏名称" />
-        </el-form-item>
-        <el-form-item label="有效投注" prop="effectiveBet">
-          <el-input v-model="form.effectiveBet" placeholder="请输入有效投注" />
-        </el-form-item>
-        <el-form-item label="总投注额" prop="allBet">
-          <el-input v-model="form.allBet" placeholder="请输入总投注额" />
-        </el-form-item>
-        <el-form-item label="派彩金额(不包含本金)" prop="profit">
-          <el-input v-model="form.profit" placeholder="请输入派彩金额(不包含本金)" />
-        </el-form-item>
-        <el-form-item label="下注时间" prop="betTime">
-          <el-date-picker clearable
-            v-model="form.betTime"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="请选择下注时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="下注时间美东" prop="wagersDate">
-          <el-date-picker clearable
-            v-model="form.wagersDate"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="请选择下注时间美东">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="结算时间" prop="settleTime">
-          <el-date-picker clearable
-            v-model="form.settleTime"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="请选择结算时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="结算时间美东" prop="modifiedDate">
-          <el-date-picker clearable
-            v-model="form.modifiedDate"
-            type="date"
-            value-format="yyyy-MM-dd"
-            placeholder="请选择结算时间美东">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="局号gameCode" prop="serialId">
-          <el-input v-model="form.serialId" placeholder="请输入局号gameCode" />
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
 import { listAgRecord, getAgRecord, delAgRecord, addAgRecord, updateAgRecord } from "@/api/lottery/agRecord";
+import { dateFormat,pickerOptions } from '@/utils/auth'
 
 export default {
   name: "AgRecord",
@@ -304,57 +134,30 @@ export default {
     return {
       // 遮罩层
       loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
       // 显示搜索条件
       showSearch: true,
       // 总条数
       total: 0,
       // AG注单表格数据
       agRecordList: [],
-      // 弹出层标题
-      title: "",
-      // 是否显示弹出层
-      open: false,
-      // 1:视讯 2:电子 3:捕鱼时间范围
-      daterangeBetTime: [],
-      // 1:视讯 2:电子 3:捕鱼时间范围
-      daterangeSettleTime: [],
       // 查询参数
       queryParams: {
         pageNum: 1,
         pageSize: 10,
         userName: null,
-        platUserName: null,
         orderNo: null,
-        gameId: null,
         gameName: null,
-        effectiveBet: null,
-        allBet: null,
-        profit: null,
-        betTime: null,
-        wagersDate: null,
-        settleTime: null,
-        modifiedDate: null,
-        resultStatus: null,
-        serialId: null,
-        gameType: null,
+        gameType: "",
       },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-        userName: [
-          { required: true, message: "用户名不能为空", trigger: "blur" }
-        ],
-        platUserName: [
-          { required: true, message: "平台用户名不能为空", trigger: "blur" }
-        ],
-      }
+      // 时间
+      dateRange:[],
+      pickerOptions: pickerOptions,
+      gameList: [
+        { label: '全部', value: ''},
+        { label: '真人视讯', value: 1},
+        { label: '电子游戏', value: 2},
+        { label: '捕鱼', value: 3},
+      ],//游戏类型
     };
   },
   created() {
@@ -364,49 +167,11 @@ export default {
     /** 查询AG注单列表 */
     getList() {
       this.loading = true;
-      this.queryParams.params = {};
-      if (null != this.daterangeBetTime && '' != this.daterangeBetTime) {
-        this.queryParams.params["beginBetTime"] = this.daterangeBetTime[0];
-        this.queryParams.params["endBetTime"] = this.daterangeBetTime[1];
-      }
-      if (null != this.daterangeSettleTime && '' != this.daterangeSettleTime) {
-        this.queryParams.params["beginSettleTime"] = this.daterangeSettleTime[0];
-        this.queryParams.params["endSettleTime"] = this.daterangeSettleTime[1];
-      }
-      listAgRecord(this.queryParams).then(response => {
+      listAgRecord(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
         this.agRecordList = response.rows;
         this.total = response.total;
         this.loading = false;
       });
-    },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        id: null,
-        userName: null,
-        platUserName: null,
-        orderNo: null,
-        gameId: null,
-        gameName: null,
-        effectiveBet: null,
-        allBet: null,
-        profit: null,
-        betTime: null,
-        wagersDate: null,
-        settleTime: null,
-        modifiedDate: null,
-        resultStatus: null,
-        serialId: null,
-        gameType: null,
-        createTime: null,
-        updateTime: null
-      };
-      this.resetForm("form");
     },
     /** 搜索按钮操作 */
     handleQuery() {
@@ -415,52 +180,8 @@ export default {
     },
     /** 重置按钮操作 */
     resetQuery() {
-      this.daterangeBetTime = [];
-      this.daterangeSettleTime = [];
       this.resetForm("queryForm");
       this.handleQuery();
-    },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.id)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加AG注单";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const id = row.id || this.ids
-      getAgRecord(id).then(response => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改AG注单";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.id != null) {
-            updateAgRecord(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            addAgRecord(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
-          }
-        }
-      });
     },
     /** 删除按钮操作 */
     handleDelete(row) {
